7270676804ddfff9be76dae0a8b373c6a28a41be
nouranmostafa235 <nourmostafa2197@gmail.com>
2026-02-21 16:24:57 +0200

    updates

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,19 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+  {
+    path: 'doctor-profile/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'doctor-profile/:id/material',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'doctor-profile/:id/office-hours',
+    renderMode: RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
